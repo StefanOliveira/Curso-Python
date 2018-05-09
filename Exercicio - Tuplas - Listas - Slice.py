11661ec2d76466d9ab7e862ee81e560bb4393cef
Stefan de Oliveira <carlos.stefan86@yahoo.com.br>
2018-05-08 22:36:30 -0300

    Delete Exercicio - Tuplas - Listas - Slice.py

--- Exercicio - Tuplas - Listas - Slice.py
+++
@@ -1,9 +0,0 @@
-#Tuplas são imutaveis - Listas podem ser editadas
-
-#exercicio Tupla
-meses = ('janeiro','fevereiro','março','abril','maio','junho','julho','agosto','setembro','outubro','novembro','dezembro')
-data_nasc = input('Digite sua data de nascimento no formato DD-MM-AAAA: ')
-indice = int(data_nasc[3:5])-1
-mes = meses[indice]
-
-print('Você nasceu no mês de ',mes)
